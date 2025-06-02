6faf5503a1fc692418bd9fd33d92a108e48c3261
komalmali1312 <komalmali1312@gmail.com>
2025-06-02 18:00:34 +0530

    Updated in assignment6

--- src/assignments/Assignment6_Variables.java
+++ src/assignments/Assignment6_Variables.java
@@ -7,15 +7,15 @@ public static void main(String[] args) {
 //1. Create two arrays to store student names ["Suresh","Mahesh","Naresh"] and marks [75, 80, 82]		
 String[] Names={"Suresh","Mahesh","Naresh"};
 int[] Marks= {75, 80, 82};
-
+int a=10;
 //2.Add 10 marks to each students using assignment operators and store it into another array
-int[] Marks2= new int[3];
-Marks2[0]=Marks[0]+10;
-Marks2[1]=Marks[1]+10;
-Marks2[2]=Marks[2]+10;
+
+Marks[0]+=a;
+Marks[1]+=a;
+Marks[2]+=a;
 
 //3.identify the average marks of all students
-float avg=(Marks2[0]+Marks2[1]+Marks2[2])/3;
+float avg=(Marks[0]+Marks[1]+Marks[2])/3;
 
 //Expected Output:
 //Updated Marks:
@@ -24,9 +24,9 @@ float avg=(Marks2[0]+Marks2[1]+Marks2[2])/3;
 //Naresh: 92
 //Average Marks: 89.0
 System.out.println("Updated Marks:");
-System.out.println(Names[0]+": "+Marks2[0]);
-System.out.println(Names[1]+": "+Marks2[1]);
-System.out.println(Names[2]+": "+Marks2[2]);
+System.out.println(Names[0]+": "+Marks[0]);
+System.out.println(Names[1]+": "+Marks[1]);
+System.out.println(Names[2]+": "+Marks[2]);
 System.out.println("Average Marks:"+avg);
 	}
 
