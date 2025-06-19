d5555f54335fc127a6accdee3cf405c9cd9aba43
komalmali1312 <komalmali1312@gmail.com>
2025-06-19 18:05:19 +0530

    Assignment 15,16,17,18 added

--- src/assignments/Test.java
+++ src/assignments/Test.java
@@ -1,61 +1,50 @@
 package assignments;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+
+
 
 public class Test {
-
+static boolean isPrime(int n)
+{
+if(n<2)
+	return false;
+for(int i=2;i*i<=n;i++)
+{
+	if(n%i==0)
+		return false;
+}
+return true;
+}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-/*Map<String,Map<String,String>>Projectdetails=new HashMap<>();
-Map<String,String>Student=new HashMap<>();
-Student.put("Name","Komal");
-Student.put("Salary","80K");
-Projectdetails.put("Komal", Student);
-System.out.println(Projectdetails.get("Komal").get("Salary"));*/
-/*String input="Komal is Good Engineer";
-String[] sepword=input.split(" ");
-String reverse="";
-for(int j=sepword.length-1;j>=0;j--)
-{
-for(int i=sepword[j].length()-1;i>=0;i--)
-{
-	reverse=reverse+sepword[j].charAt(i);
-}
-reverse=reverse+" ";
-}
-System.out.println(reverse);*/
-
-/*String s="I Love Java Mongo Ruby Java Python";
-HashMap<Character,Integer>maptest=new HashMap<>();
-for(int i=0;i<s.length()-1;i++)	
-{ 
-	char c=s.charAt(i);
-	if(maptest.containsKey(c))
-	
-		maptest.put(c,maptest.get(c)+1);	
-	
-	else
-		maptest.put(c,1);	
-}
-	
-System.out.println(maptest);*/
-String test="I Love Java Mongo Ruby Java Python";
-String[] sepword=test.split(" ");
-Set<String>myset=new HashSet<>();
-for(int i=0;i<sepword.length-1;i++)
-{
-	String demo=sepword[i];
-	if(myset.contains(demo))
+	//Fibbonnacy series 
+	int a=0,b=1,fiboutput=0,count=10;
+	for(int i=0;i<count;i++)
 	{
-		System.out.println("Duplivcate:"+demo);	
+		fiboutput=a+b;
+		a=b;
+		b=fiboutput;
+	
+System.out.print(fiboutput+" ");
 	}
-	else
-		myset.add(demo);
-}
-System.out.println(myset);
+	//prime number
+		if(isPrime(12))
+				System.out.println("Yes it is prime");
+		else
+			System.out.println("Yes it is not prime");
+		
+		//Palindrome string check
+		String s="Madam".toLowerCase();
+		String reverse="";
+		for(int i=s.length()-1;i>=0;i--)
+		{
+		reverse=reverse+s.charAt(i);
+		}
+		System.out.println(reverse);
+		if(reverse.equals(s))
+			System.out.println("It is Palindrome");
+		else
+			System.out.println("It is Not Palindrome");
+		
 	}
 
 }
